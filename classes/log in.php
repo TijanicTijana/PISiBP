61ab30421625400d2b53aba770b322524bdf39cb
TijanicTijana <151183736+TijanicTijana@users.noreply.github.com>
2024-03-05 23:35:39 +0100

    Create log in.php

---
+++ classes/log in.php
@@ -0,0 +1,63 @@
+<?php
+	
+
+class Login
+{
+
+	private $error="";
+
+	public function evaluate($data)
+	{
+
+		$Korisnik_Email=addslashes($data['Korisnik_Email']); //ako je u email adresi neki karakter koji bi php gledao kao simbol koji nije deo teksta kao npr' automatski dodaje \ i taj simbol posmatra kao deo teksta
+		$Korisnik_Sifra=addslashes($data['Korisnik_Sifra']);
+
+		$query = "select * from korisnik where Korisnik_Email = '$Korisnik_Email' limit 1";
+		
+		// echo $query;
+		$DB=new Database();
+		$result = $DB->read($query);
+		// print_r($result);
+
+		if($result)
+		{
+			$row = $result[0];
+
+			if ($Korisnik_Sifra == $row['Korisnik_Sifra'])
+			{
+				//kreiraj sesiju podataka
+				$_SESSION['Korisnik_ID']=$row['Korisnik_ID'];
+			}
+			else
+			{
+				$this->error .= "Pogresna sifra<br>";
+			}
+		}
+		else
+		{
+			$this->error .= "Pogresan email<br>";
+		}
+		return $this->error;
+	}
+
+	public function check_login($Korisnik_ID)
+	{
+		$query = "select Korisnik_ID from korisnik where Korisnik_ID = '$KorisnikID' limit 1";
+		
+		// echo $query;
+		$DB=new Database();
+		$result = $DB->read($query);
+		// print_r($result);
+
+		if($result)
+		{
+			return true;
+		}
+
+		return false;
+
+	}
+
+}
+
+?>

20e6745e39b381d2424ac0b322d5207f8a67b888
TijanicTijana <151183736+TijanicTijana@users.noreply.github.com>
2024-03-14 21:28:53 +0100

    Update user.php

--- classes/user.php
+++ classes/user.php
@@ -20,6 +20,21 @@ class User
 			return false;
 		}
 	}
+	public function get_user($id)
+	{
+		$query = "select * from korisnik where Korisnik_ID = $id limit 1";
+		$DB=new Database();
+		$result= $DB->read($query);
+
+		if($result)
+		{
+			return $result[0];
+		}
+		else
+		{
+			return false;
+		}
+	}
 }
 
 ?>
